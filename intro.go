a0308e50b5d188d4f72aa92d98132a69af30f90b
mariam <yegetamariam@gmail.com>
2023-08-09 10:16:46 +0300

    Class 03 Basic Types Average of numbers

--- intro.go
+++ intro.go
@@ -1,12 +1,6 @@
 package main
 
 func main() {
-	// convert()
-	// primitives()
-	// coonstants()
-	// fmt.Printf("%v,declared at Package Level", ExportedConst)
-	// fmt.Printf("%v,NotVisible outside function local constant",notVisible)
-	// arrr()
-	slicess()
+	average()
 
 }
