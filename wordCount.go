226801ede537e9df6383a275c3229de127d7a765
Mariam Gustavo <yegetamariam@gmail.com>
2023-08-10 14:31:28 +0300

    added the total counts for the files

--- wordCount.go
+++ wordCount.go
@@ -11,6 +11,7 @@ func main() {
 	wordCount()
 }
 func wordCount() {
+	var totallc, totalcc, totalwc int
 	for _, fileName := range os.Args[1:] {
 		var lc, cc, wc int
 		file, err := os.Open(fileName)
@@ -27,7 +28,12 @@ func wordCount() {
 			wc += len(strings.Fields(s))
 
 		}
+		totallc += lc
+		totalcc += cc
+		totalwc += wc
 		fmt.Printf("%5d %5d %5d %s\n", lc, cc, wc, fileName)
 		file.Close()
 	}
+	fmt.Printf("%5d %5d %5d total\n", totallc, totalcc, totalwc)
+
 }
